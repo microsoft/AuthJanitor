853e6e2e11b01b2f762f96a5986227f9e0edd337
Anthony Turner <225599+anthturner@users.noreply.github.com>
2020-05-15 08:33:41 -0700

    Add provider for Azure Event Hubs

---
+++ AuthJanitor.Providers.EventHub/EventHubKeyConfiguration.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using System.ComponentModel;
+
+namespace AuthJanitor.Providers.EventHub
+{
+    public class EventHubKeyConfiguration : AuthJanitorProviderConfiguration
+    {
+        /// <summary>
+        /// Duplication of Service Bus "Policykey" enumeration to avoid passing through a dependency
+        /// </summary>
+        public enum EventHubKeyTypes
+        {
+            Primary,
+            Secondary
+        }
+
+        /// <summary>
+        /// Kind (type) of Event Hub Key
+        /// </summary>
+        [DisplayName("Event Hub Key Type")]
+        public EventHubKeyTypes KeyType { get; set; }
+
+        /// <summary>
+        /// Event Hub Authorization Rule name
+        /// </summary>
+        [DisplayName("Event Hub Namespace")]
+        public string NamespaceName { get; set; }
+
+        /// <summary>
+        /// Event Hub Authorization Rule name
+        /// </summary>
+        [DisplayName("Authorization Rule")]
+        public string AuthorizationRuleName { get; set; }
+
+        /// <summary>
+        /// Skip the process of scrambling the other (non-active) key
+        /// </summary>
+        [DisplayName("Skip Scrambling Other Key?")]
+        [Description("If checked, the opposite key (e.g. primary/secondary) will NOT be scrambled at the end of the rekeying")]
+        public bool SkipScramblingOtherKey { get; set; }
+    }
+}
