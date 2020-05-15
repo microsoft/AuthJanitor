5d28832f9927a77c6345e13a9bbab238f48aa2dc
Anthony Turner <225599+anthturner@users.noreply.github.com>
2020-05-14 17:39:30 -0700

    Add Azure Redis Cache provider (#51)

---
+++ AuthJanitor.Providers.RedisCache/RedisCacheKeyConfiguration.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using System.ComponentModel;
+
+namespace AuthJanitor.Providers.Redis
+{
+    public class RedisCacheKeyConfiguration : AuthJanitorProviderConfiguration
+    {
+        public enum RedisKeyTypes
+        {
+            [Description("Primary")]
+            Primary,
+            [Description("Secondary")]
+            Secondary
+        }
+
+        /// <summary>
+        /// Kind (type) of Redis Key
+        /// </summary>
+        [DisplayName("Key Kind")]
+        [Description("Kind of Redis Key to manage")]
+        public RedisKeyTypes KeyType { get; set; }
+
+        /// <summary>
+        /// Skip the process of scrambling the other (non-active) key
+        /// </summary>
+        [DisplayName("Skip Scrambling Other Key?")]
+        [Description("If checked, the opposite key (e.g. primary/secondary) will NOT be scrambled at the end of the rekeying")]
+        public bool SkipScramblingOtherKey { get; set; }
+    }
+}
---
+++ AuthJanitor.Providers.RedisCache/RedisCacheKeyRekeyableObjectProvider.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using AuthJanitor.Extensions.Azure;
+using Microsoft.Azure.Management.Redis.Fluent;
+using Microsoft.Azure.Management.Redis.Fluent.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AuthJanitor.Providers.Redis
+{
+    [Provider(Name = "Redis Cache Key",
+              IconClass = "fa fa-database",
+              Description = "Regenerates a Master Key for a Redis Cache instance")]
+    [ProviderImage(ProviderImages.REDIS_SVG)]
+    public class RedisCacheKeyRekeyableObjectProvider : RekeyableObjectProvider<RedisCacheKeyConfiguration>
+    {
+        private readonly ILogger _logger;
+
+        public RedisCacheKeyRekeyableObjectProvider(ILogger<RedisCacheKeyRekeyableObjectProvider> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<RegeneratedSecret> GetSecretToUseDuringRekeying()
+        {
+            // NOTE: Redis Fluent lacks async methods
+            _logger.LogInformation("Getting temporary secret to use during rekeying from other ({0}) key...", GetOtherKeyType(Configuration.KeyType));
+            var redisCache = await RedisCache;
+            IRedisAccessKeys keys = redisCache.GetKeys();
+            _logger.LogInformation("Successfully retrieved temporary secret!");
+            return new RegeneratedSecret()
+            {
+                Expiry = DateTimeOffset.UtcNow + TimeSpan.FromMinutes(10),
+                UserHint = Configuration.UserHint,
+                NewSecretValue = GetKeyValue(keys, GetOtherKeyType(Configuration.KeyType))
+            };
+        }
+
+        public override async Task<RegeneratedSecret> Rekey(TimeSpan requestedValidPeriod)
+        {
+            // NOTE: Redis Fluent lacks async methods
+            _logger.LogInformation("Regenerating Redis Cache key type {0}", Configuration.KeyType);
+            var redisCache = await RedisCache;
+            var keys = redisCache.RegenerateKey(GetKeyType(Configuration.KeyType));
+
+            _logger.LogInformation("Successfully rekeyed Redis Cache key type {0}", Configuration.KeyType);
+            return new RegeneratedSecret()
+            {
+                Expiry = DateTimeOffset.UtcNow + requestedValidPeriod,
+                UserHint = Configuration.UserHint,
+                NewSecretValue = GetKeyValue(keys, GetKeyType(Configuration.KeyType))
+            };
+        }
+
+        public override async Task OnConsumingApplicationSwapped()
+        {
+            // NOTE: Redis Fluent lacks async methods
+            if (!Configuration.SkipScramblingOtherKey)
+            {
+                _logger.LogInformation("Scrambling Redis Cache key kind {0}", GetOtherKeyType(Configuration.KeyType));
+                (await RedisCache).RegenerateKey(GetOtherKeyType(Configuration.KeyType));
+            }
+            else
+                _logger.LogInformation("Skipping scrambling CosmosDB key kind {0}", GetOtherKeyType(Configuration.KeyType));
+        }
+
+        public override IList<RiskyConfigurationItem> GetRisks()
+        {
+            List<RiskyConfigurationItem> issues = new List<RiskyConfigurationItem>();
+            if (Configuration.SkipScramblingOtherKey)
+            {
+                issues.Add(new RiskyConfigurationItem()
+                {
+                    Score = 80,
+                    Risk = $"The other (unused) Redis Cache Key of this type is not being scrambled during key rotation",
+                    Recommendation = "Unless other services use the alternate key, consider allowing the scrambling of the unused key to 'fully' rekey Redis Cache and maintain a high degree of security."
+                });
+            }
+
+            return issues;
+        }
+
+        public override string GetDescription() =>
+            $"Regenerates the {Configuration.KeyType} key for a Redis Cache instance " +
+            $"called '{ResourceName}' (Resource Group '{ResourceGroup}'). " +
+            $"The {GetOtherKeyType(Configuration.KeyType)} key is used as a temporary " +
+            $"key while rekeying is taking place. The {GetOtherKeyType(Configuration.KeyType)} " +
+            $"key will {(Configuration.SkipScramblingOtherKey ? "not" : "also")} be rotated.";
+
+        private Task<IRedisCache> RedisCache => this.GetAzure().ContinueWith(az => az.Result.RedisCaches.GetByResourceGroupAsync(ResourceGroup, ResourceName)).Unwrap();
+
+        private RedisKeyType GetKeyType(RedisCacheKeyConfiguration.RedisKeyTypes keyType) => keyType switch
+        {
+            RedisCacheKeyConfiguration.RedisKeyTypes.Primary => RedisKeyType.Primary,
+            RedisCacheKeyConfiguration.RedisKeyTypes.Secondary => RedisKeyType.Secondary,
+            _ => throw new System.Exception($"Key type '{keyType}' not implemented")
+        };
+
+        private RedisKeyType GetOtherKeyType(RedisCacheKeyConfiguration.RedisKeyTypes keyType) => keyType switch
+        {
+            RedisCacheKeyConfiguration.RedisKeyTypes.Primary => RedisKeyType.Secondary,
+            RedisCacheKeyConfiguration.RedisKeyTypes.Secondary => RedisKeyType.Primary,
+            _ => throw new System.Exception($"Key type '{keyType}' not implemented")
+        };
+
+        private string GetKeyValue(IRedisAccessKeys keys, RedisKeyType keyType) => keyType switch
+        {
+            RedisKeyType.Primary => keys.PrimaryKey,
+            RedisKeyType.Secondary => keys.SecondaryKey,
+            _ => throw new System.Exception($"Key type '{keyType}' not implemented")
+        };
+    }
+}
