4708c62cb1f1ceece753bb7d1b68f725c3b75290
Anthony Turner <225599+anthturner@users.noreply.github.com>
2020-05-20 10:52:35 -0700

    fix license headers

--- AuthJanitor.Providers.Azure/AzureApplicationLifecycleProvider.cs
+++ AuthJanitor.Providers.Azure/AzureApplicationLifecycleProvider.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AuthJanitor.Providers.Azure
--- AuthJanitor.Providers.Azure/Workflows/TwoKeyAzureRekeyableObjectProvider.cs
+++ AuthJanitor.Providers.Azure/Workflows/TwoKeyAzureRekeyableObjectProvider.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
