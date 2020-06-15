94ba778a64bb13b593dd3558e78b479ca5ab2ed0
renato <remarcia@microsoft.com>
2020-06-15 19:30:35 -0400

    Added Standard Header

--- AuthJanitor.Tests/JsonConverterTests.cs
+++ AuthJanitor.Tests/JsonConverterTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+using System;
 using System.Text.Json;
 using AuthJanitor.Integrations.IdentityServices.AzureActiveDirectory;
 using Xunit;
