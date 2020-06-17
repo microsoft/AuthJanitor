ad1779a1dba4418e77dfc7844302525789a1f3e8
Peter Bryntesson <pbrynt@microsoft.com>
2020-06-17 13:46:46 +0200

    Added a data store for entity framework core including unit tests

---
+++ src/AuthJanitor.Integrations.DataStores.EntityFrameworkCore/Configurations/ResourceConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AuthJanitor.UI.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthJanitor.Integrations.DataStores.EntityFrameworkCore.Configurations
+{
+    public class ResourceConfiguration : IEntityTypeConfiguration<Resource>
+    {
+        public void Configure(EntityTypeBuilder<Resource> builder)
+        {
+            builder.HasKey(x => x.ObjectId);
+            // TODO which properties are required?
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.IsRekeyableObjectProvider).IsRequired();
+            builder.Property(x => x.ProviderType).IsRequired();
+        }
+    }
+}
