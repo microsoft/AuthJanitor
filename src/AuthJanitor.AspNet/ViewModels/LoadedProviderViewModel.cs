1a1479e1936bc6b12026ea802f5d0b1eba1e4419
Anthony Turner <225599+anthturner@users.noreply.github.com>
2020-11-10 12:43:31 -0500

    Remove ALC/RKO providers from front end (#99)

--- src/AuthJanitor.AspNet/ViewModels/LoadedProviderViewModel.cs
+++ src/AuthJanitor.AspNet/ViewModels/LoadedProviderViewModel.cs
@@ -23,7 +23,6 @@ namespace AuthJanitor.UI.Shared.ViewModels
     {
         public string OriginatingFile { get; set; }
         public string ProviderTypeName { get; set; }
-        public bool IsRekeyableObjectProvider { get; set; }
         public ProviderAttribute Details { get; set; }
         public IEnumerable<ProviderCapabilities> Capabilities { get; set; } = new[] { ProviderCapabilities.None };
         public string AssemblyVersion { get; set; }
